78974ab073c1eba3c871ea0408806f6db7699f80
djsookz <djdimcho25@gmail.com>
2025-03-09 18:12:06 +0200

    second deploy

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,12 @@
-import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import { getAuth, signOut, onAuthStateChanged } from 'firebase/auth';
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import Navigation from '../components/Navigation';
 import './HomePage.css'
 
 const HomePage = () => {
   const [user, setUser] = useState<any>(null);
-  const navigate = useNavigate();
+ 
   const auth = getAuth();
 
   useEffect(() => {
@@ -20,14 +19,7 @@ const HomePage = () => {
     });
   }, [auth]);
 
-  const handleSignOut = async () => {
-    try {
-      await signOut(auth); // Извършваме излизането
-      navigate('/'); // След излизането пренасочваме към началната страница
-    } catch (error) {
-      console.error("Sign out error:", error);
-    }
-  };
+  
 
   return (
     <div className='Home-Container' >
