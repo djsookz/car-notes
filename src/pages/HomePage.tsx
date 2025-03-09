bc3ea99b131c43b5c2327386c5b4cb49844a8e5e
djsookz <djdimcho25@gmail.com>
2025-03-09 18:24:21 +0200

    second deploy

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,34 @@
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { useState, useEffect } from 'react';
+import Navigation from '../components/Navigation';
+import './HomePage.css'
 
 const HomePage = () => {
+  const [user, setUser] = useState<any>(null);
+ 
+  const auth = getAuth();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (currentUser) => {
+      if (currentUser) {
+        setUser(currentUser);
+      } else {
+        setUser(null);
+        console.log(user)
+      }
+    });
+  }, [auth]);
+
+  
+
   return (
-    <div>
-      <h1>Добре дошли!</h1>
+    <div className='Home-Container' >
+      <Navigation />
+      <h1>Добре дошли в сайта!</h1>
+      
+
+
+      
     </div>
   );
 };
