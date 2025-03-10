15caa2b7f853a5f26c754c04cef3725cb2cd732d
Dimo Dimov <djdimcho25@gmail.com>
2025-03-10 16:19:35 +0200

    add statistics

--- src/pages/StatisticsPage.tsx
+++ src/pages/StatisticsPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import Navigation from "../components/Navigation";
 import { db, collection, getDocs } from "../firebase/config";
 import {
