f8ebbee3974823eb63564bb2f071fe2965330dbb
Dimo Dimov <djdimcho25@gmail.com>
2025-05-08 21:53:35 +0300

    fixing nav

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,27 +1,36 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { getAuth, signOut } from "firebase/auth";
 import "./Navigation.css";
 
 const Navigation = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
   const auth = getAuth();
   const user = auth.currentUser;
 
   const handleSignOut = async () => {
     try {
-      await signOut(auth); // Извършваме излизането
-      window.location.href = "/"; // Пренасочваме към началната страница
+      await signOut(auth);
+      window.location.href = "/";
     } catch (error) {
       console.error("Sign out error:", error);
     }
   };
 
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
-    <div className="nav-container">
+    <div className={`nav-container ${menuOpen ? "open" : ""}`}>
       <div className="nav-logo">
         <h2>LOGO</h2>
       </div>
-
-      <div className="nav-buttons">
+  
+      
+      <button className="hamburger-menu" onClick={toggleMenu}>{menuOpen ? '×' : '☰'}</button>
+  
+      <div className={`nav-buttons ${menuOpen ? "active" : ""}`}>
         <Link to="/home" className="nav-link">
           <button className="nav-button">Начало</button>
         </Link>
@@ -41,11 +50,13 @@ const Navigation = () => {
           <button className="nav-button">Графика</button>
         </Link>
       </div>
-      <div className="profile-container">
+  
+      {/* Преместено тук */}
+      <div className={`profile-container ${menuOpen ? "active" : ""}`}>
         {user && user.photoURL ? (
           <img src={user.photoURL} alt="Profile" className="profile-image" />
         ) : (
-          <div className="default-image">П</div> // Default профилна снимка (инициал)
+          <div className="default-image">П</div>
         )}
         <span className="username">{user ? user.displayName : "Гост"}</span>
         <button onClick={handleSignOut} className="sign-out-button">
@@ -54,6 +65,7 @@ const Navigation = () => {
       </div>
     </div>
   );
+  
 };
 
 export default Navigation;
